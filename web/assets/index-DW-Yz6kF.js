b42f458d512aa7906eb46e8bb21f237dfd608346
GNUTUX <salehoukiki@gmail.com>
2026-04-19 14:46:58 +0100

    Add files via upload

---
+++ web/assets/index-DW-Yz6kF.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-sfuwvzF6.js","./index-DNzFMgCo.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-BeKTTXA7.js";import{registerPlugin as t}from"./index-DNzFMgCo.js";const _=t("Share",{web:()=>e(()=>import("./web-sfuwvzF6.js"),__vite__mapDeps([0,1]),import.meta.url).then(r=>new r.ShareWeb)});export{_ as Share};
