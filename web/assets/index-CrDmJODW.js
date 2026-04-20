fa4224d487a5a9fafda2934d0ca459090a76fd68
GNUTUX <salehoukiki@gmail.com>
2026-04-20 19:37:57 +0100

    Add files via upload

---
+++ web/assets/index-CrDmJODW.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-cGH_ByOa.js","./index-DNzFMgCo.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-CU22D8pb.js";import{registerPlugin as p}from"./index-DNzFMgCo.js";const i=p("App",{web:()=>e(()=>import("./web-cGH_ByOa.js"),__vite__mapDeps([0,1]),import.meta.url).then(r=>new r.AppWeb)});export{i as App};
